dee3dc73b62aabcc78e13ba141a82df5af2b527e
oen142 <oen142@naver.com>
2021-04-29 21:21:08 +0900

    Initial commit

---
+++ src/main/java/com/wani/fastcampusspringtutorial/FastcampusSpringTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.wani.fastcampusspringtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastcampusSpringTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastcampusSpringTutorialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wani/fastcampusspringtutorial/FastcampusSpringTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wani.fastcampusspringtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastcampusSpringTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
